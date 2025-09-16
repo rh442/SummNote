b3cfd43114a4b2d1a5e07c4efa480a7fb996096b
rh442 <hraymond6442@gmail.com>
2025-09-15 23:11:26 -0400

    github actions

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/Project/" : "/",
+  base: '/SummNote/',
   plugins: [react()],
   server: {
     watch: {
