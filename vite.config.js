3f6c3e676eeca1b2080c03a7cd90c0ef18767989
rh442 <hraymond6442@gmail.com>
2025-12-22 15:23:37 -0500

    Fix asset paths for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SummNote/',
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100,
-    },
-  },
+  base: '/', // Deploy at root
 })
